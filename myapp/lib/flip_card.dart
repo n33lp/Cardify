62a1d8ecf777a23dcf3d0ed45ff80d2053bb2510
n33lp <canada0706@gmail.com>
2024-05-14 23:51:28 -0600

    ui fix

--- myapp/lib/flip_card.dart
+++ myapp/lib/flip_card.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
 import 'document.dart';
 import 'questionanswer.dart';
-import 'folder_contents.dart'; // Import the FolderContents widget you defined earlier
-import 'folder.dart'; // Import the Folder class
+import 'folder_contents.dart';
+import 'folder.dart';
 
 class FlipCardPage extends StatefulWidget {
   final Document document;
   final Folder folder;
   final Folder trashFolder;
 
-  FlipCardPage(
-      {Key? key,
-      required this.document,
-      required this.folder,
-      required this.trashFolder})
-      : super(key: key);
+  FlipCardPage({
+    Key? key,
+    required this.document,
+    required this.folder,
+    required this.trashFolder,
+  }) : super(key: key);
 
   @override
   _FlipCardPageState createState() => _FlipCardPageState();
@@ -24,18 +24,25 @@ class FlipCardPage extends StatefulWidget {
 class _FlipCardPageState extends State<FlipCardPage> {
   late Folder currentFolder;
   late Folder trashFolder;
+  late PageController _pageController;
+  late int currentPage;
+
   @override
   void initState() {
     super.initState();
     currentFolder = widget.folder;
-    trashFolder = widget.trashFolder; // Initialize trashFolder from the widget
+    trashFolder = widget.trashFolder;
+    _pageController = PageController(
+        viewportFraction:
+            0.8); // Adjust viewport fraction for better swiping experience
+    currentPage = 0;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Q Cards'),
+        title: Text('Questions'),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
@@ -60,34 +67,131 @@ class _FlipCardPageState extends State<FlipCardPage> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemCount: widget.document.questions.length,
-        itemBuilder: (context, index) {
-          QuestionAnswer qa = widget.document.questions[index];
-          return Card(
-            child: FlipCard(
-              direction: FlipDirection.HORIZONTAL,
-              front: Container(
-                padding: EdgeInsets.all(10),
-                child: Center(
-                  child: Text(qa.question),
-                ),
-              ),
-              back: Container(
-                padding: EdgeInsets.all(10),
-                child: Center(
-                  child: Text(qa.answer),
-                ),
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: widget.document.questions.length,
+              onPageChanged: (index) {
+                setState(() {
+                  currentPage = index;
+                });
+              },
+              itemBuilder: (context, index) {
+                QuestionAnswer qa = widget.document.questions[index];
+                return Card(
+                  child: FlipCard(
+                    direction: FlipDirection.HORIZONTAL,
+                    front: Container(
+                      padding: EdgeInsets.all(30),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize
+                              .min, // Use 'min' to reduce excess space and center the column itself
+                          children: [
+                            Text(
+                              "Question",
+                              textAlign: TextAlign
+                                  .center, // Center the text "Question" horizontally
+                              style: TextStyle(
+                                fontSize:
+                                    20, // Set the font size for the header
+                                fontWeight:
+                                    FontWeight.bold, // Make the header bold
+                              ),
+                            ),
+                            SizedBox(
+                                height:
+                                    8), // Provide some spacing between the texts
+                            Text(
+                              qa.question,
+                              textAlign: TextAlign
+                                  .center, // Center the actual question horizontally
+                              style: TextStyle(
+                                fontSize:
+                                    16, // Set the font size for the question
+                                color: Colors
+                                    .black54, // Set the text color, assuming a light background
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    back: Container(
+                      padding: EdgeInsets.all(30),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize
+                              .min, // Use 'min' to reduce excess space and center the column itself
+                          children: [
+                            Text(
+                              "Answer",
+                              textAlign: TextAlign
+                                  .center, // Center the text "Question" horizontally
+                              style: TextStyle(
+                                fontSize:
+                                    20, // Set the font size for the header
+                                fontWeight:
+                                    FontWeight.bold, // Make the header bold
+                              ),
+                            ),
+                            SizedBox(
+                                height:
+                                    8), // Provide some spacing between the texts
+                            Text(
+                              qa.answer,
+                              textAlign: TextAlign
+                                  .center, // Center the actual question horizontally
+                              style: TextStyle(
+                                fontSize:
+                                    16, // Set the font size for the question
+                                color: Colors
+                                    .black54, // Set the text color, assuming a light background
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+          Container(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.document.questions.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    _pageController.animateToPage(
+                      index,
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.easeIn,
+                    );
+                  },
+                  child: Container(
+                    width: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: currentPage == index ? Colors.blue : Colors.grey,
+                    ),
+                    child: Text("${index + 1}"),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 
   void fetchQuestions() async {
-    // Function to fetch or regenerate the document's questions
     Document newDocument =
         await Document.load(); // Assuming Document.load() fetches new data
     setState(() {
