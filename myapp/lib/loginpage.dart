414d97b8051b0962b291ce63b992e14c545087fc
n33lp <canada0706@gmail.com>
2024-05-15 20:55:04 -0600

    add login page and remove dead code on main.dart

---
+++ myapp/lib/loginpage.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'folder.dart';
+import 'folder_contents.dart';
+import 'package:flutter/services.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  late Map<String, dynamic> apiData; //apiData['LOGIN_URL']
+
+  @override
+  void initState() {
+    super.initState();
+    loadApiData();
+  }
+
+  Future<void> login() async {
+    var url = apiData['LOGIN_URL'];
+
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'username': _usernameController.text,
+        'password': _passwordController.text,
+      }),
+    );
+    print(response);
+    if (response.statusCode == 200) {
+      Folder rootFolder = Folder(
+        name: "Root",
+        createDate: DateTime.now(),
+        lastEditedDate: DateTime.now(),
+        documents: [],
+        subfolders: [],
+        isStarred: false,
+      );
+      Folder trashFolder = Folder(
+        name: "Trash",
+        createDate: DateTime.now(),
+        lastEditedDate: DateTime.now(),
+        documents: [],
+        subfolders: [],
+        isStarred: false,
+      );
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => FolderContents(
+                  folder: rootFolder, trashFolder: trashFolder)));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Invalid username or password')),
+      );
+    }
+  }
+
+  Future<Map<String, dynamic>> readJson() async {
+    final String response =
+        await rootBundle.loadString('creds/USER_CREDS.json');
+    final data = await json.decode(response);
+    return data;
+  }
+
+  Future<void> loadApiData() async {
+    apiData = await readJson();
+    print('API Data loaded: $apiData');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Cardify Login!',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 40),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: login,
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 18),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'folder.dart'; // Import the Folder class you defined earlier
 import 'document.dart'; // Import the Document class you defined earlier
-import 'folder_contents.dart'; // Import the FolderContents widget you defined earlier
+import 'folder_contents.dart';
+import 'loginpage.dart'; // Import the FolderContents widget you defined earlier
 
 void main() {
   runApp(MyApp());
@@ -10,27 +11,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Folder rootFolder = Folder(
-      name: "Root",
-      createDate: DateTime.now(),
-      lastEditedDate: DateTime.now(),
-      documents: [],
-      subfolders: [],
-      isStarred: false,
-    );
-    Folder trashFolder = Folder(
-      name: "Trash",
-      createDate: DateTime.now(),
-      lastEditedDate: DateTime.now(),
-      documents: [],
-      subfolders: [],
-      isStarred: false,
-    );
     return MaterialApp(
       title: 'Document Manager',
-      home: FolderContents(
-          folder: rootFolder,
-          trashFolder: trashFolder), // Start with the root folder
+      home: LoginPage(), // Start with the Login Page
     );
   }
 }
